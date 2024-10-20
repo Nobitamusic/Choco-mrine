5949f318319e127c07088739ef8abc79f95e0032
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-10-20 21:12:59 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/MOON-CODES/Extra-Plugin",
+    "https://github.com/Choco-criminal/New-vps",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
