1977913c1a582dad95f11ca01aec4d05bc522b95
Nobitamusic <ssibasundar94@gmail.com>
2024-10-22 22:39:42 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -70,7 +70,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002384476626"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "1002342282754"))
 
 
 # Your User ID.
